96526e348aa2f52422da02602d575ebfc9c6da46
juliana1thome <juliana1thome@gmail.com>
2021-10-08 09:56:45 -0400

    Add Binary Serialization

---
+++ BinarySerialization/BinarySerialization/DataSerializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerialization
+{
+    // It will serialize the data and deserialize it too
+    public class DataSerializer
+    {
+        public void BinarySerialized(object data, string filePath)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+         
+            // If this file exist delete it and create a new one
+            if (File.Exists(filePath)) File.Delete(filePath);
+            
+            // Create a variable called fileStream(that is a FileStream),
+            // which will be created in the filePath from the file deleted
+            var fileStream = File.Create(filePath);
+            
+            // Serialize this file that you created / The data is the data that will be serialized
+            binaryFormatter.Serialize(fileStream, data);
+            
+            // Don't forget to close it
+            fileStream.Close();
+        }
+
+        public object BinaryDeserialize(string filePath)
+        {
+            object obj = null;
+
+            FileStream fileStream;
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            // Same thing the file exists open it
+            // and read it after make the object be a deserialized file, and close the file stream
+            if (File.Exists(filePath))
+            {
+                fileStream = File.OpenRead(filePath);
+                obj = binaryFormatter.Deserialize(fileStream);
+                fileStream.Close();
+            }
+            
+            return obj;
+        }
+    }
+}
---
+++ BinarySerialization/BinarySerialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerialization
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Creating person object...
+            Person person = new Person(){FirstName = "Juliana", LastName =  "Caldeira"};
+            
+            // Save this in a file called
+            string filePath = "data.save";
+
+            // Create a new data serializer
+            DataSerializer dataSerializer = new DataSerializer();
+
+            // Make the person object that has information become a data serialized
+            dataSerializer.BinarySerialized(person, filePath);
+            
+            // Now deserialize it as new var that will be the deserialized object from the class Person
+            var personObject = dataSerializer.BinaryDeserialize(filePath) as Person;
+            
+            // Printing...
+            Console.WriteLine(personObject.FirstName);
+            Console.WriteLine(personObject.LastName);
+        }
+
+    }
+}
